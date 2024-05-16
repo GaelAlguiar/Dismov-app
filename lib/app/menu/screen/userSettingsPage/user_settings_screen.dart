7959c8ab530dde3fd5506e3d7e5dae35c0d9f5ff
JesusGaMo <jesusga7864@gmail.com>
2024-05-16 13:11:00 -0600

    Creacion de interfaz de perfil solo

--- lib/app/menu/screen/userSettingsPage/user_settings_screen.dart
+++ lib/app/menu/screen/userSettingsPage/user_settings_screen.dart
@@ -18,10 +18,15 @@ class UserSettingsScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('User Profile'),
+        title: const Text('Perfil'),
         backgroundColor: AppColor.yellow,
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+          IconButton(
+            onPressed: () {
+              
+            }, 
+            icon: const Icon(Icons.edit)
+          )
         ],
       ),
       body: const _UserSettingsView(),
@@ -71,18 +76,18 @@ class __UserSettingsState extends State<_UserSettingsView> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: EdgeInsets.only(top: 10),
+                padding: EdgeInsets.only(top: 0),
                 child: Row(
                   children: [
                     SizedBox(
-                      height: 10,
+                      height: 0,
                       width: 5,
                     ),
                   ],
                 ),
               ),
               SizedBox(
-                height: 3,
+                height: 0,
               ),
             ],
           ),
@@ -108,28 +113,129 @@ class __UserSettingsState extends State<_UserSettingsView> {
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text(name.toString()),
+                    //Text(name.toString()),
                     colocarImagen(profilePhoto.toString()),
-                    Text(email.toString()),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      name.toString(),
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 7,
+                    ),
+                    Text(
+                      email.toString(),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
                     const SizedBox(
                       height: 25,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(15, 0, 15, 25),
-                      child: CustomFilledButton(
-                        text: "Cerrar Sesión",
-                        buttonColor: AppColor.darker,
-                        onPressed: () async {
-                          await LoginGoogleUtils().signOutGoogle();
-                          await LoginGoogleUtils().singOutWithEmail();
-                          if (FirebaseAuth.instance.currentUser == null) {
-                            if (context.mounted) {
-                              context.go("/login");
-                            }
-                          }
-                        },
+                    Container(
+                      width: 350,
+                      height: 80,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 52, 143, 217), 
+                            width: 2, 
+                          ),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Text(
+                              'Dog lover.\n21 years old.',
+                              style: TextStyle(
+                              fontSize: 17,
+                            ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          'Preferencias de mascota',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () {
+                            // Acción de editar preferencias
+                          },
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Chip(
+                          label: Text('Pequeño'),
+                        ),
+                        SizedBox(width: 8),
+                        Chip(
+                          label: Text('Activo'),
+                        ),
+                        SizedBox(width: 8),
+                        Chip(
+                          label: Text('Perro'),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 15, 25),
+                      child: SizedBox(
+                        width: 200,
+                        height: 47,
+                        child: CustomFilledButton(
+                          text: "Cerrar Sesión",
+                          buttonColor: AppColor.darker,
+                          onPressed: () async {
+                            await LoginGoogleUtils().signOutGoogle();
+                            await LoginGoogleUtils().singOutWithEmail();
+                            if (FirebaseAuth.instance.currentUser == null) {
+                              if (context.mounted) {
+                                context.go("/login");
+                              }
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    /*Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 0, 15, 25),
+                      child: CustomFilledButton(
+                        text: "Editar perfil",
+                          buttonColor: AppColor.darker,
+                          onPressed: () async {
+                            
+                          },
+                      ),
+                    )*/
                   ]),
             ),
           );
@@ -143,14 +249,14 @@ colocarImagen(String url) {
   if (url == 'null') {
     return Image.network(
       'https://cdn-icons-png.flaticon.com/512/3541/3541871.png',
-      width: 100,
-      height: 100,
+      width: 190,
+      height: 190,
     );
   } else {
     return Image.network(
       url,
-      width: 100,
-      height: 100,
+      width: 190,
+      height: 190,
     );
   }
 }
